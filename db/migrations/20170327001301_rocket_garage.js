72bb1425726dccfa4c77d3d1c3cc92a13518fa0f
dustwise <jared.mohney@gmail.com>
2017-03-27 00:38:24 -0600

    building database structure, migration added

---
+++ db/migrations/20170327001301_rocket_garage.js
@@ -0,0 +1,53 @@
+exports.up = function(knex, Promise) {
+
+  return Promise.all([
+
+    knex.schema.createTable('users', function(table){
+      table.increments('user_id').primary();
+      table.string('steamID').unique();
+    }),
+
+    knex.schema.createTable('user_items', function(table){
+      table.increments('id').primary();
+      table.integer('user_id')
+            .references('user_id')
+            .inTable('users');
+      table.integer('item')
+            .references('item_id')
+            .inTable('items');
+      table.integer('paint')
+            .references('paint_id')
+            .inTable('paints');
+      table.integer('cert')
+            .references('cert_id')
+            .inTable('certs');
+    }),
+
+    knex.schema.createTable('items', function(table){
+      table.increments('item_id').primary();
+      table.string('item_type');
+      table.string('item_name').unique();
+    }),
+
+    knex.schema.createTable('paints', function(table){
+      table.increments('paint_id').primary();
+      table.string('paint_color').unique();
+    }),
+
+    knex.schema.createTable('paints', function(table){
+      table.increments('cert_id').primary();
+      table.string('cert_type').unique();
+    })
+
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('user_items'),
+    knex.schema.dropTable('items'),
+    knex.schema.dropTable('paints'),
+    knex.schema.dropTable('certs')
+  ]);
+};
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express           = require('express'),
+      parser            = require('body-parser'),
+      passport          = require('passport'),
+      SteamStrategy     = require('../../').Strategy,
+      db                = require ('./db');
+
+const app               = express(),
+      jsonParser        = parser.json();
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
